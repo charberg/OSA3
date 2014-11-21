4a7dbc50f7742b972eb22d5b8f62e8ad30f4743e
Charles Bergeron <charlesbergeron@cmail.carleton.ca>
2014-11-21 12:38:19 -0500

    Added database initialization and account struct

--- partB.c
+++ partB.c
@@ -2,9 +2,23 @@
 #include "stdio.h"
 #include "string.h"
 
+FILE* db;	//"Database" file
+
+typedef struct account {
+
+	int accNum;
+	int PIN;
+	float balance;
+
+} ACCOUNT;
+
 int main() {
 
-	//Test commit
+	db = fopen("db.txt","w");
+
+	//Database initialization
+
+	fprintf(db, "00001,107,3443.22\n00011,323,10089.97\n00117,259,112.00\n");
 	
 	return 0;
 }
